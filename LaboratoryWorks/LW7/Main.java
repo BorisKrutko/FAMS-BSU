7ccd1bef88a69a4c34152c79bfb6b86ad038902f
Boris Krutko <bakrutko@gmail.com>
2024-11-09 17:28:22 +0300

    5.28/9/11/24

--- LaboratoryWorks/LW7/Main.java
+++ LaboratoryWorks/LW7/Main.java
@@ -1,7 +1,21 @@
 package LW7;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        new MyFramePart1();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("part1 or part2 (input 1 or 2): ");
+        String inputString = scanner.nextLine();
+        switch (inputString) {
+            case "1":
+                new MyFramePart1();
+                break;
+            case "2":
+                new MyFramePart2();
+                break;
+            default:
+                throw new AssertionError();
+        }
     }
 }
