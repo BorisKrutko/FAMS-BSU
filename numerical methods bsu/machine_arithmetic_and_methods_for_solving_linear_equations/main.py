3d6b0172744f2069f3b89c5892239cdebc9d6aef
bakru_b9pmn2k <bakrutko@gmail.com>
2025-10-10 15:06:49 +0300

    Refactor LU decomposition and solve methods for improved clarity and performance; update input/output filenames in main script.

--- numerical methods bsu/machine_arithmetic_and_methods_for_solving_linear_equations/main.py
+++ numerical methods bsu/machine_arithmetic_and_methods_for_solving_linear_equations/main.py
@@ -1,4 +1,4 @@
 from lu_matrix_processor import LUMatrixProcessor
 
 processor = LUMatrixProcessor()
-processor.linalg_solve("data/in2.txt", "data/out2.txt")
+processor.linalg_solve("data/in3.txt", "data/out3.txt")
