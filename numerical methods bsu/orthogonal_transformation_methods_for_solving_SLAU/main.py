3495fc6bb63450837ed54aacf4a6d6751e91c861
bakru_b9pmn2k <bakrutko@gmail.com>
2025-10-27 23:41:47 +0300

    Refactor main.py: Update input arrays for OrthogonalTransformations and add new test cases
    - Modified existing input arrays (a1, d1, c1, b1) for the first transformation.
    - Introduced two new sets of input arrays (a2, d2, c2, b2) and (a3, d3, c3, b3) for additional transformations.
    - Implemented calls to straight_move and reverse_stroke methods for new test cases.
    - Added print statements to display results and residual norms for all transformations.

--- numerical methods bsu/orthogonal_transformation_methods_for_solving_SLAU/main.py
+++ numerical methods bsu/orthogonal_transformation_methods_for_solving_SLAU/main.py
@@ -1,10 +1,10 @@
 from orthogonal_transformation_methods import OrthogonalTransformations
 import numpy as np
 
-a1 = np.array([3, 2, 1])
-d1 = np.array([2, 5, 3, 2])
-c1 = np.array([1, 1, 1])
-b1 = np.array([4, 3, -4, 7])
+a1 = np.array([2, -2, -2])
+d1 = np.array([-2, 3, 4, 3])
+c1 = np.array([2, 5, 1])
+b1 = np.array([2,23,12,6])
 
 
 cl = OrthogonalTransformations(a1, d1, c1, b1)
@@ -13,3 +13,28 @@ cl.reverse_stroke()
 cl.print_x()
 print(cl.get_residual_norm(a1, d1, c1, b1))
 
+
+a2 = np.array([-4, 2, -2, 5, -5, -3, 0, 0, 5])
+d2 = np.array([4, -6, 4, 3, -2, 1, -6, 2, 3, -4])
+c2 = np.array([-3, 4, -4, -1, 4, -4, 0, 4, 2])
+b2 = np.array([1,-6,2,-9,-11,-53,-9,6,5,1])
+
+
+cl = OrthogonalTransformations(a2, d2, c2, b2)
+cl.straight_move()
+cl.reverse_stroke()
+cl.print_x()
+print(cl.get_residual_norm(a2, d2, c2, b2))
+
+
+a3 = np.array([1, 5, -2, 0, 3, 0, 3, 5, -3, -3, 3, 0, 4, 1, -1, -1])
+d3 = np.array([5, 3, 6, -6, 6, -2, 3, 5, 6, -2, -6, 5, 2, 4, 1, -2, 4 ])
+c3 = np.array([-3, 2, -1, 3, -4, 0, -5, -1, 3, 2, -1, -4, -1, 5, -1, -2])
+b3 = np.array([-1,13,24,-15,6,3,-19,52,124,-25,-108,41,12,183,13,-81,52])
+
+
+cl = OrthogonalTransformations(a3, d3, c3, b3)
+if cl.straight_move(): 
+    cl.reverse_stroke()
+    cl.print_x()
+    print(cl.get_residual_norm(a3, d3, c3, b3))
